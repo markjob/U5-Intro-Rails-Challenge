3be863e811ab7240efe6b6531a0f5bdaa18d91d1
Mark Job <markjob24@gmail.com>
2013-10-25 09:34:50 -0500

    Final commit before submission

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :price, :stock_quantity, :title
+  validates_presence_of :title
+  validates :description, :presence => true
+  validates :price, :presence => true
+  validates :stock_quantity, :presence => true
 end
